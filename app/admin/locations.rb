3c9607159c959a18de311f5ba471928cc1b5bd19
Raghu Betina <raghu@firstdraft.com>
2019-12-11 22:45:33 +0000

    Whitelist columns in Location for ActiveAdmin

--- app/admin/locations.rb
+++ app/admin/locations.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Location do
+
+ permit_params :city, :indooroutdoor
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
