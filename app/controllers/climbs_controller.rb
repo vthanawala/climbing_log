a22d28057d53995df4af0df380a099475e74e5c9
Raghu Betina <raghu@firstdraft.com>
2019-12-11 22:45:55 +0000

    Only climb user can modify climb

--- app/controllers/climbs_controller.rb
+++ app/controllers/climbs_controller.rb
@@ -1,4 +1,14 @@
 class ClimbsController < ApplicationController
+  before_action :current_user_must_be_climb_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_climb_user
+    climb = Climb.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == climb.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @climbs = Climb.all
 
