e83433d8a5367923e7c32fbc9d3ada6d63a45b86
Raghu Betina <raghu@firstdraft.com>
2019-12-11 22:45:30 +0000

    Whitelist columns in Climb for ActiveAdmin

--- app/admin/climbs.rb
+++ app/admin/climbs.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Climb do
+
+ permit_params :user_id, :location_id, :rating, :picture
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
